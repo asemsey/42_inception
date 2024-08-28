7b64d6fd46c74cebc84a07386a2df0a3a0a80a37
Anna Semsey <asemsey@2-G-9.42heilbronn.de>
2024-08-28 17:38:25 +0200

    v2 all dockerfiles and config

---
+++ v2/srcs/requirements/wordpress/conf/wp-config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd /var/www/html
+
+rm -rf ./*
+
+wp core download --allow-root
+
+wp config create	--allow-root --force \
+	--url="$WP_URL" \
+	--dbname="$SQL_DATABASE" \
+	--dbuser="$SQL_USER" \
+	--dbpass="$SQL_PASSWORD" \
+	--dbhost="mariadb:3306"
+
+wp core install	--allow-root \
+	--url="$WP_URL" \
+	--title="$WP_TITLE" \
+	--admin_user="$WP_ADMIN" \
+	--admin_password="$WP_ADMIN_PASS" \
+	--admin_email="$WP_ADMIN_MAIL" \
+	--skip-email
+
+wp user create	--allow-root \
+	$WP_USER_NAME $WP_USER_EMAIL \
+	--user_pass="$WP_USER_PASS"
+
+php-fpm7.3 -F
